16223b6947809d76aa0538ad12e6cf9fafcb5fb6
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-03-18 08:18:59 +0530

    Create Solution.py

---
+++ 3070-Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-K/Solution.py
@@ -0,0 +1,86 @@
+"""
+
+## 🚀 AlgoDiary | LeetCode Solutions by Gopal Kushwaha
+
+🧠 Problem: 3070. Count Submatrices with Top-Left Element and Sum ≤ k  
+🔗 Platform: LeetCode  
+🏷 Difficulty: Medium  
+---------------------
+
+💡 Problem Statement:
+
+You are given a 0-indexed integer matrix grid and an integer k.
+
+Return the number of submatrices that:
+
+• Contain the top-left element (0,0)  
+• Have a sum less than or equal to k  
+
+----------------------------------------------
+
+📌 Example
+
+Input: grid = [[7,6,3],[6,6,1]] , k = 18  
+
+Output: 4  
+
+----------------------------------------------
+
+💡 Approach (2D Prefix Sum Optimization)
+
+Steps:
+
+• Traverse the matrix row by row  
+• Maintain a prefix array to store cumulative column sums  
+• For each row:
+    - Compute running row sum  
+    - Add it to prefix[j] (this gives submatrix sum from (0,0) to (i,j))  
+
+• If prefix[j] ≤ k → valid submatrix → increment count  
+
+This avoids recomputing sums and gives optimal performance.
+
+----------------------------------------------
+
+✨ Why Prefix Sum?
+
+• Converts O(n^3) brute force into O(n * m)  
+• Efficiently calculates submatrix sum in constant time  
+• Most important pattern for 2D matrix problems  
+
+----------------------------------------------
+
+⏱ Time Complexity : O(n * m)  
+📦 Space Complexity: O(m)
+
+----------------------------------------------
+
+✍️ Author: Gopal Kushwaha  
+📚 Repository: AlgoDiary-LeetCode-Notes  
+
+=======================================
+
+"""
+
+# Prefix Sum (Optimal Approach)
+
+class Solution:
+    def countSubmatrices(self, grid, k):
+        n = len(grid)
+        m = len(grid[0])
+
+        prefix = [0] * m
+        ans = 0
+
+        for i in range(n):
+            rowSum = 0
+
+            for j in range(m):
+                rowSum += grid[i][j]
+                prefix[j] += rowSum
+
+                # check valid submatrix
+                if prefix[j] <= k:
+                    ans += 1
+
+        return ans
