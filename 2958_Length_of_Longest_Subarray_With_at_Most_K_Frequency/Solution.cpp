e795490c9eed8cea41a717bb86c2df28fcf8501f
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-08-12 10:42:57 +0530

    Create Solution.cpp

---
+++ 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/Solution.cpp
@@ -0,0 +1,95 @@
+# /*
+## 🚀 AlgoDiary | LeetCode Solutions by Gopal Kushwaha
+
+🧠 Problem: 2958. Length of Longest Subarray With at Most K Frequency  
+🔗 Platform: LeetCode  
+🏷 Difficulty: Medium
+
+---
+
+💡 Problem Statement:
+
+You are given an integer array `nums` and an integer `k`.
+
+A subarray is called **good** if the frequency of every element in the subarray is less than or equal to `k`.
+
+Return the length of the **longest good subarray**.
+
+---
+
+💡 Approach (Sliding Window + HashMap)
+
+Steps:
+
+• Take an `unordered_map` to store the frequency of each element.  
+• Use two pointers `i` and `j` to maintain the sliding window.  
+• Add `nums[i]` to the window and increase its frequency.  
+• If the frequency of the current element becomes greater than `k`, shrink the window from the left.  
+• Move pointer `j` forward and decrease the frequency of `nums[j]`.  
+• Continue until the frequency becomes valid again.  
+• Update `maxLen` using the current window length.  
+• Return the maximum length found.
+
+Why it works?
+
+The sliding window always maintains a good subarray where every element appears at most `k` times.
+
+Whenever the frequency of an element exceeds `k`, we shrink the window from the left until the condition becomes valid again.
+
+Each element is added and removed from the window at most once, making the solution efficient.
+
+---
+
+⏱ Time Complexity : O(n)
+
+📦 Space Complexity: O(n)
+
+---
+
+✍️ Author: Gopal Kushwaha
+
+📚 Repository: AlgoDiary-LeetCode-Notes
+
+=======================================
+
+\*/
+
+# C++ Solution
+
+```cpp
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+
+        // taking HashMap for freq count
+        unordered_map<int, int> map;
+
+        int i = 0;
+        int j = 0;
+        int maxLen = 0;
+
+        // Apply sliding window
+        while (i < nums.size()) {
+
+            int ele = nums[i];
+
+            map[ele]++;
+
+            // Reduce the window when freq of ele > k
+            while (map[ele] > k) {
+
+                int frontEle = nums[j];
+
+                map[frontEle]--;
+                j++;
+            }
+
+            // Update the maxLen
+            maxLen = max(maxLen, (i - j) + 1);
+
+            i++;
+        }
+
+        return maxLen;
+    }
+};
